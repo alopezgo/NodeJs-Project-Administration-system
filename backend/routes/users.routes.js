4ed1d852b816ece147a2c570df848fb043a08511
jvera <jvera@sentra.cl>
2023-05-10 22:36:24 -0400

    Login: crea servicio y ruta de login básico

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -5,6 +5,7 @@ const { UsersController } = require('../controllers');
 
 
 //api.get('/v1/user/authenticate/auth', UsersController.authenticate)
+api.post('/v1/user/login', UsersController.loginUser);
 api.get('/v1/userTest', UsersController.getUser);
 api.post('/v1/user/createUser', UsersController.postCreatUser);
 
