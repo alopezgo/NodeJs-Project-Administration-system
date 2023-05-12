351e76c9e23950654452a6eabfa7d1d08cb2ae38
Andrea <alopezgo1991@gmail.com>
2023-05-12 00:35:27 -0400

    Se agregan mayores validaciones al login

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,59 +1,86 @@
-const { pool } = require('../db/db');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { pool } = require("../db/db");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.getUser = async (req, res) => {
   try {
-    const query = 'SELECT * FROM sac.usuario';
+    const query = "SELECT * FROM sac.usuario";
     const result = await pool.query(query);
 
-    console.log("BD DATA: ", result.rows)
+    console.log("BD DATA: ", result.rows);
 
     return res.status(200).send({
       success: true,
       message: "usuario encontrado",
-      Data: result.rows
-    })
-
+      Data: result.rows,
+    });
   } catch (error) {
-    console.error('Error al obtener los usuarios', error);
-    res.status(500).send('Error en el servidor');
+    console.error("Error al obtener los usuarios", error);
+    res.status(500).send("Error en el servidor");
   }
 };
 
 exports.postCreatUser = async (req, res) => {
   try {
-
-      //funcionalidades crear usuario
+    //funcionalidades crear usuario
 
     return res.status(200).send({
       success: true,
       message: "usuario encontrado",
-      Data: result.rows
-    })
-
+      Data: result.rows,
+    });
   } catch (error) {
-    console.error('Error al obtener los usuarios', error);
-    res.status(500).send('Error en el servidor');
+    console.error("Error al obtener los usuarios", error);
+    res.status(500).send("Error en el servidor");
   }
 };
 
 async function login(correo, contraseña) {
-  const query =`SELECT  correo, contrasena 
+  const query = `SELECT  id, id_rol, correo, contrasena 
                    FROM   sac.usuario 
                    WHERE  correo = $1 
-                   AND    contrasena = $2
-                   AND id_rol in (1,2)`;
+                   AND id_estado = 1`;
 
-  const { rows } = await pool.query(query, [correo, contraseña]);
+  const { rows } = await pool.query(query, [correo]);
+  const isPasswordCorrect = await bcrypt.compare(
+    contraseña,
+    rows[0].contrasena
+  );
+  // Genera un token JWT para el usuario
+  const token = jwt.sign(
+    { userId: rows[0].id },
+    "tu_secreto_secreto",
+    {
+      expiresIn: "1h",
+    }
+  );
+  console.log(token);
 
   if (rows.length === 0) {
-    return { success: false, message: 'Correo o contraseña incorrectos', data: null };
+    return {
+      success: false,
+      message: "El correo ingresado no se encuentra registrado",
+      data: null,
+    };
   }
-  return { success: true, message: 'Login exitoso', data: rows };
-}
+  if (rows[0].id_rol == 3) {
+    return {
+      success: false,
+      message: "No cuenta con los permisos necesarios para ingresar al sitio",
+      data: null,
+    };
+  }
+  if (!isPasswordCorrect) {
+    return {
+      success: false,
+      message: "La contraseña ingresada es incorrecta",
+      data: null,
+    };
+  }
 
+  return { success: true, message: "Login exitoso", data: rows };
+}
 
 exports.loginUser = async (req, res) => {
   try {
@@ -65,13 +92,12 @@ exports.loginUser = async (req, res) => {
       return res.status(404).json(resultado);
     }
   } catch (error) {
-    console.error('Error con la conexión a la base de datos', error);
+    console.error("Error con la conexión a la base de datos", error);
     res.status(500).send(error);
   }
 };
 
-// const otrafuncion = async () => {
-//   console.log(await login('adminempresa1@gmail.com', 'pass123'));
-// }
-// otrafuncion();
-
+const otrafuncion = async () => {
+  console.log(await login("test3@mail.com", "test3"));
+};
+otrafuncion();
