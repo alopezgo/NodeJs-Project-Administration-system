06f6ddfbac0b7722c6c24b174ff3e794dbde48a1
Andrea <alopezgo1991@gmail.com>
2023-05-18 21:52:33 -0400

    se agregan vistas

--- webFrontend/assets/js/dashboard.js
+++ webFrontend/assets/js/dashboard.js
@@ -7,52 +7,52 @@
   feather.replace({ 'aria-hidden': 'true' })
 
   // Graphs
-  const ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      plugins: {
-        legend: {
-          display: false
-        },
-        tooltip: {
-          boxPadding: 3
-        }
-      }
-    }
-  })
+  // const ctx = document.getElementById('myChart')
+  // // eslint-disable-next-line no-unused-vars
+  // const myChart = new Chart(ctx, {
+  //   type: 'line',
+  //   data: {
+  //     labels: [
+  //       'Sunday',
+  //       'Monday',
+  //       'Tuesday',
+  //       'Wednesday',
+  //       'Thursday',
+  //       'Friday',
+  //       'Saturday'
+  //     ],
+  //     datasets: [{
+  //       data: [
+  //         15339,
+  //         21345,
+  //         18483,
+  //         24003,
+  //         23489,
+  //         24092,
+  //         12034
+  //       ],
+  //       lineTension: 0,
+  //       backgroundColor: 'transparent',
+  //       borderColor: '#007bff',
+  //       borderWidth: 4,
+  //       pointBackgroundColor: '#007bff'
+  //     }]
+  //   },
+  //   options: {
+  //     plugins: {
+  //       legend: {
+  //         display: false
+  //       },
+  //       tooltip: {
+  //         boxPadding: 3
+  //       }
+  //     }
+  //   }
+  // })
 })()
 
-const id_empresa = localStorage.getItem('id_empresa');
-console.log("id_empresa", id_empresa)
+// const id_empresa = localStorage.getItem('id_empresa');
+// console.log("id_empresa", id_empresa)
 
 document.addEventListener("DOMContentLoaded", function () {
   const nombreUsuario = localStorage.getItem("nombre_usuario");
