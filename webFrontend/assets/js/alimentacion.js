4278e4412f992df087cb6905581db82e301721f5
Andrea <alopezgo1991@gmail.com>
2023-05-20 14:22:35 -0400

    Agrega Formulario Filtro Informes en Alimentación

--- webFrontend/assets/js/alimentacion.js
+++ webFrontend/assets/js/alimentacion.js
@@ -7,7 +7,19 @@
 
 })();
 
+window.onload = function () {
+  var fecha = new Date(); //Fecha actual
+  var mes = fecha.getMonth() + 1; //obteniendo mes
+  var dia = fecha.getDate(); //obteniendo dia
+  var ano = fecha.getFullYear(); //obteniendo año
+  if (dia < 10) dia = "0" + dia; //agrega cero si el menor de 10
+  if (mes < 10) mes = "0" + mes; //agrega cero si el menor de 10
+  document.getElementById("fechaDesde").value = ano + "-" + mes + "-" + dia;
+  document.getElementById("fechaHasta").value = ano + "-" + mes + "-" + dia;
+};
+
 document.addEventListener("DOMContentLoaded", function () {
+
   const nombreUsuario = localStorage.getItem("nombre_usuario");
 
   if (nombreUsuario) {
@@ -20,4 +32,5 @@ document.addEventListener("DOMContentLoaded", function () {
     nombreUsuarioElemento.appendChild(imagenElemento);
     nombreUsuarioElemento.appendChild(nombreElemento);
   }
+
 });
