6bebd1a676933e97296f921ebc62eb4af928f273
Andrea <alopezgo1991@gmail.com>
2023-05-21 17:40:19 -0400

    Se aplica estilo a vista alimentación y se limpia codigo de alimentacion.js

--- webFrontend/assets/js/alimentacion.js
+++ webFrontend/assets/js/alimentacion.js
@@ -1,16 +1,46 @@
-/* globals Chart:false, feather:false */
-
-(() => {
-  "use strict";
-
-  feather.replace({ "aria-hidden": "true" });
-
-})();
-
-
 document.addEventListener("DOMContentLoaded", function () {
+  //Se establecen opciones de la tabla y se llama a función Getconsumo
+  var tabla_consumo = $("#example").DataTable({
+    language: {
+      url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+    },
+    columns: [
+      { title: "Empleado", data: "nom_empleado" },
+      { title: "Rut", data: "rut" },
+      { title: "Consumo", data: "tipo_consumo" },
+      { title: "Fecha", data: "fecha" },
+      { title: "Hora", data: "hora" },
+      { title: "Precio", data: "precio" },
+    ],
+  });
+  GetConsumos();
 
-  const nombreUsuario = localStorage.getItem("nombre_usuario");
+  // Función Auxiliar para Obtener lista de Consumos por Empresa
+  function GetConsumos() {
+    const id_empresa = localStorage.getItem("id_empresa");
+    return $.ajax({
+      url: "http://localhost:3000/api/v1/consumos/" + id_empresa,
+      type: "GET",
+      success: function (response) {
+        const res = response;
+        const consumo = res.data;
+        consumo.forEach((fila) => {
+          tabla_consumo.row
+            .add({
+              nom_empleado: fila.nom_empleado,
+              rut: fila.rut,
+              tipo_consumo: fila.tipo_consumo,
+              fecha: fila.fecha,
+              hora: fila.hora,
+              precio: fila.precio,
+            })
+            .draw();
+        });
+      },
+    });
+  }
+
+  //Se rellenan fechas de inputs tipo select en Formulario
   const id_empresa = localStorage.getItem("id_empresa");
   const url_centros = "http://localhost:3000/api/v1/centrocosto/" + id_empresa;
   const url_tiposConsumo = "http://localhost:3000/api/v1/tiposconsumo";
@@ -19,31 +49,32 @@ document.addEventListener("DOMContentLoaded", function () {
   cargarOptions(url_centros, centroSelect);
   cargarOptions(url_tiposConsumo, consumoSelect);
 
-  window.onload = function () {
-    var fecha = new Date(); //Fecha actual
-    var mes = fecha.getMonth() + 1; //obteniendo mes
-    var dia = fecha.getDate(); //obteniendo dia
-    var ano = fecha.getFullYear(); //obteniendo año
-    if (dia < 10) dia = "0" + dia; //agrega cero si el menor de 10
-    if (mes < 10) mes = "0" + mes; //agrega cero si el menor de 10
-    document.getElementById("fechaDesde").value = ano + "-" + mes + "-" + dia;
-    document.getElementById("fechaHasta").value = ano + "-" + mes + "-" + dia;
-  };
+  //Se rellenan opciones de inputs tipo date en Formulario
+  var fecha = new Date(); //Fecha actual
+  var mes = fecha.getMonth() + 1; //obteniendo mes
+  var dia = fecha.getDate(); //obteniendo dia
+  var ano = fecha.getFullYear(); //obteniendo año
+  if (dia < 10) dia = "0" + dia; //agrega cero si el menor de 10
+  if (mes < 10) mes = "0" + mes; //agrega cero si el menor de 10
+  document.getElementById("fechaDesde").value = ano + "-" + mes + "-" + dia;
+  document.getElementById("fechaHasta").value = ano + "-" + mes + "-" + dia;
 
-  if (nombreUsuario) {
-    const nombreUsuarioElemento = document.getElementById("nombre-usuario");
+  //Se rellena menu lateral con Nombre del Usuario
+  const nombreUsuario = localStorage.getItem("nombre_usuario");
+  const nombreUsuarioElemento = document.getElementById("nombre-usuario");
+  const imagenElemento = document.getElementById("img-usuario");
+  const nombreElemento = document.createElement("span");
 
-    const imagenElemento = document.getElementById("img-usuario");
-    const nombreElemento = document.createElement("span");
+  if (nombreUsuario != "") {
     nombreElemento.innerHTML = "Hola " + nombreUsuario;
-
-    nombreUsuarioElemento.appendChild(imagenElemento);
-    nombreUsuarioElemento.appendChild(nombreElemento);
+  } else {
+    nombreElemento.innerHTML = "Mi cuenta";
   }
-
+  nombreUsuarioElemento.appendChild(imagenElemento);
+  nombreUsuarioElemento.appendChild(nombreElemento);
 });
 
-// Función para cargar options en Select.
+// Función auxiliar para cargar options en Select.
 async function cargarOptions(url, select) {
   try {
     // Obtiene los Centros
@@ -69,30 +100,51 @@ async function cargarOptions(url, select) {
     if (url === "http://localhost:3000/api/v1/tiposconsumo") {
       // Agrega las opciones al select.
       select.innerHTML = "";
+      let newOption = new Option(
+        (text = "Ninguno"),
+        (value = "0"),
+        (defaultSelected = true),
+        (selected = true)
+      );
+      select.add(newOption);
       opciones.forEach((opcion) => {
         const option = document.createElement("option");
         option.value = opcion.id_tipo_consumo; // Modificación aquí
         const str = opcion.tipo_consumo;
         const str2 = str.charAt(0).toUpperCase() + str.slice(1);
         option.textContent = str2;
-        select.appendChild(option);
+        select.add(option);
       });
     } else {
+      // Agrega las opciones al select.
       select.innerHTML = "";
+      let newOption = new Option(
+        (text = "Ninguno"),
+        (value = "0"),
+        (defaultSelected = true),
+        (selected = true)
+      );
+      select.add(newOption);
       opciones.forEach((opcion) => {
         const option = document.createElement("option");
         option.value = opcion.id; // Modificación aquí
         option.textContent = opcion.centro;
-        select.appendChild(option);
+        select.add(option);
       });
-
-    } 
-    
+    }
   } catch (error) {
     console.error(error);
     alert("Error al cargar las opciones");
   }
 }
 
+//Se invoca funcion para cargar iconos de menu lateral rapidamente
+/* globals Chart:false, feather:false */
+(() => {
+  "use strict";
+
+  feather.replace({ "aria-hidden": "true" });
+})();
+
 //Conectar Formulario con servicio http://localhost:3000/api/v1/consumos/:id_empresa/:ccosto/:desde/:hasta
-// 
+//
