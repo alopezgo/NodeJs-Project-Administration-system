29202779f668429f6aeaeb3dae04c9ffe8b6ce2b
Andrea <alopezgo1991@gmail.com>
2023-05-21 23:22:49 -0400

    modifica servicio getAsistenciaPorEmpresa como queryparams

--- backend/controllers/asistencias.controller.js
+++ backend/controllers/asistencias.controller.js
@@ -1,8 +1,8 @@
-const { pool } = require('../db/db');
+const { pool } = require("../db/db");
 
-exports.getDetalleAsistencia= async (req, res) => {
-    try {
-        const query = `
+exports.getDetalleAsistencia = async (req, res) => {
+  try {
+    const query = `
                     SELECT e.nombre||' '||e.apellido_paterno||' '||e.apellido_materno AS nom_empleado,
                         e.rut||'-'||e.dv AS rut_empleado,
                         p.nombre AS nom_empresa,
@@ -17,36 +17,62 @@ exports.getDetalleAsistencia= async (req, res) => {
                         JOIN sac.centro_costos cc ON e.id = cc.id_empresa;`;
     const result = await pool.query(query);
 
-    console.log("BD DATA: ", result.rows)
+    console.log("BD DATA: ", result.rows);
 
     return res.status(200).send({
       success: true,
       message: "Detalle Asistencia encontrado",
-      Data: result.rows
-    })
-
+      Data: result.rows,
+    });
   } catch (error) {
-    console.error('Error al obtener Detalle Asistencia', error);
-    res.status(500).send('Error en el servidor');
+    console.error("Error al obtener Detalle Asistencia", error);
+    res.status(500).send("Error en el servidor");
   }
 };
 
 exports.getAsistenciaPorEmpresa = async (req, res) => {
   try {
+    const centro = req.query.centro;
+    const evento = req.query.evento;
+    const desde = req.query.desde;
+    const hasta = req.query.hasta;
     const { id_empresa } = req.params;
 
     // Ejecutar la consulta
-    const query = `
-      SELECT CONCAT(e.nombre, ' ',e.apellido_paterno,' ', e.apellido_materno) AS empleado,
-       e.rut||'-'||e.dv AS rut,
-       ea.evento AS tipo,
-       to_char(da.dt_evento, 'YYYY-MM-DD') as fecha,
-	     to_char(da.dt_evento, 'HH24:MI:SS') as hora
-       FROM sac.detalle_asistencia da JOIN sac.empleado e ON e.id = da.id_empleado
-	     JOIN sac.evento_asistencia ea ON da.id_evento_asistencia = ea.id
-	     Where e.id_empresa = $1;
-    `;
-    const { rows } = await pool.query(query, [id_empresa]);
+    let query = `
+            SELECT cc.centro as centro_costos,
+              concat(e.nombre,' ', e.apellido_paterno,' ',e.apellido_materno) as nom_empleado,
+              concat(e.rut,'-', e.dv) as rut,
+              ea.evento as tipo_asistencia,
+              to_char(da.dt_evento, 'YYYY-MM-DD') as fecha,
+              to_char(da.dt_evento, 'HH24:MI:SS') as hora
+              FROM sac.detalle_asistencia  as da 
+              JOIN sac.empleado e ON e.id = da.id_empleado
+              JOIN sac.evento_asistencia ea ON da.id_evento_asistencia = ea.id
+              JOIN sac.empresa as p ON e.id_empresa = p.id
+              JOIN sac.centro_costos as cc ON e.id_centro_costos= cc.id
+              WHERE e.id_empresa = $1;`;
+    const params = [id_empresa];
+
+    if (centro !== undefined) {
+      query += ` AND e.id_centro_costos = $${params.length + 1}`;
+      params.push(centro);
+    }
+
+    if (evento !== undefined) {
+      query += ` AND ea.id = $${params.length + 1}`;
+      params.push(evento);
+    }
+
+    if (desde !== undefined && hasta !== undefined) {
+      query += ` AND date(da.dt_evento) between $${params.length + 1} AND $${
+        params.length + 2
+      }`;
+      params.push(desde);
+      params.push(hasta);
+    }
+
+    const { rows } = await pool.query(query, params);
 
     // Devolver los resultados en formato JSON
     return res.status(200).json({
