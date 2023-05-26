31a256dd53b5b14e6d1076e6ed1bfdb04ca898f1
Andrea <alopezgo1991@gmail.com>
2023-05-26 00:07:49 -0400

    Modifica servicio asistencia para aceptar params

--- backend/controllers/asistencias.controller.js
+++ backend/controllers/asistencias.controller.js
@@ -34,19 +34,44 @@ exports.getDetalleAsistencia= async (req, res) => {
 exports.getAsistenciaPorEmpresa = async (req, res) => {
   try {
     const { id_empresa } = req.params;
+    const centro = req.query.centro;
+    const evento = req.query.evento;
+    const desde = req.query.desde;
+    const hasta = req.query.hasta;
 
     // Ejecutar la consulta
-    const query = `
-      SELECT CONCAT(e.nombre, ' ',e.apellido_paterno,' ', e.apellido_materno) AS empleado,
+    let query = `
+      SELECT
+       cc.centro as centro_costos, 
+       CONCAT(e.nombre, ' ',e.apellido_paterno,' ', e.apellido_materno) AS empleado,
        e.rut||'-'||e.dv AS rut,
        ea.evento AS tipo,
        to_char(da.dt_evento, 'YYYY-MM-DD') as fecha,
 	     to_char(da.dt_evento, 'HH24:MI:SS') as hora
-       FROM sac.detalle_asistencia da JOIN sac.empleado e ON e.id = da.id_empleado
+       FROM sac.detalle_asistencia da 
+       JOIN sac.empleado e ON e.id = da.id_empleado
 	     JOIN sac.evento_asistencia ea ON da.id_evento_asistencia = ea.id
-	     Where e.id_empresa = $1;
-    `;
-    const { rows } = await pool.query(query, [id_empresa]);
+       JOIN sac.empresa as p ON e.id_empresa = p.id
+       JOIN sac.centro_costos as cc ON e.id_centro_costos= cc.id
+	     Where e.id_empresa = $1`;
+    const params = [id_empresa];
+
+    if (centro !== undefined) {
+      query += ` AND e.id_centro_costos = $${params.length + 1}`;
+      params.push(centro);
+    }
+    if (evento !== undefined) {
+      query += ` AND ea.id = $${params.length + 1}`;
+      params.push(evento);
+    }
+    if (desde !== undefined && hasta !== undefined) {
+      query += ` AND date(da.dt_evento) between $${params.length + 1} AND $${
+        params.length + 2
+      }`;
+      params.push(desde);
+      params.push(hasta);
+    }
+    const { rows } = await pool.query(query, params);
 
     // Devolver los resultados en formato JSON
     return res.status(200).json({
@@ -56,6 +81,6 @@ exports.getAsistenciaPorEmpresa = async (req, res) => {
     });
   } catch (error) {
     console.error("Error al obtener Asistencias por Empresa", error);
-    return res.status(500).send("Error en el servidor");
+    return res.status(500).send("Error en el servidor") ;
   }
 };
