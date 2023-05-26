076c5b53d44f6c4ef5800c3c87d5ff6201d8bd01
Andrea <alopezgo1991@gmail.com>
2023-05-26 16:14:26 -0400

    Fix permisos.js

--- webFrontend/assets/js/permisos.js
+++ webFrontend/assets/js/permisos.js
@@ -1,12 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
+  if ($.fn.dataTable.isDataTable("#example")) {
+    $("#example").dataTable().fnClearTable();
+    $("#example").dataTable().fnDestroy();
+  }
+  $(document).ready(function () {
+    var tabla_permiso = $("#example").DataTable({
+      language: {
+        destroy: true,
+        url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+      },
+      columns: [
+        { title: "Empleado", data: "empleado" },
+        { title: "Rut", data: "rut" },
+        { title: "Motivo", data: "tipo" },
+        { title: "Fecha", data: "fecha" },
+        {
+          title: "Acciones",
+          defaultContent:
+            "<button class='btnEditar btn btn-success' type='button'>Editar</button>&nbsp;&nbsp;<button class='btnBorrar btn btn-danger' type='button'>Borrar</button>",
+        },
+      ],
+    });
+    GetPermisos();
+
+    // Obtener lista de permisos por Empresa
+    function GetPermisos() {
+      const id_empresa = localStorage.getItem("id_empresa");
+      return $.ajax({
+        url: "http://localhost:3000/api/v1/permisos/" + id_empresa,
+        type: "GET",
+        success: function (response) {
+          const res = response;
+          const permiso = res.data;
+          permiso.forEach((fila) => {
+            tabla_permiso.row
+              .add({
+                empleado: fila.empleado,
+                rut: fila.rut,
+                tipo: fila.tipo,
+                fecha: fila.fecha,
+              })
+              .draw();
+          });
+        },
+      });
+    }
+  });
+
   //Se rellenan fechas de inputs tipo select en Formulario
   const id_empresa = localStorage.getItem("id_empresa");
   const url_empleados = "http://localhost:3000/api/v1/empleados/" + id_empresa;
   const url_tiposPermiso = "http://localhost:3000/api/v1/tipospermiso";
-  const empleadosSelect = document.querySelector("#empleadosSelect");
+  const empleadoSelect = document.querySelector("#empleadoSelect");
   const permisoSelect = document.querySelector("#permisoSelect");
   cargarOptions(url_tiposPermiso, permisoSelect);
-  cargarOptions(url_empleados, empleadosSelect);
+  cargarOptions(url_empleados, empleadoSelect);
 
   //Se rellenan opciones de inputs tipo date en Formulario
   var fecha = new Date(); //Fecha actual
@@ -86,8 +134,8 @@ async function cargarOptions(url, select) {
       select.add(newOption);
       opciones.forEach((opcion) => {
         const option = document.createElement("option");
-        option.value = opcion.id; // Modificación aquí
-        option.textContent = opcion.centro;
+        option.value = opcion.id_empleado; // Modificación aquí
+        option.textContent = opcion.nom_empleado;
         select.add(option);
       });
     }
@@ -97,8 +145,103 @@ async function cargarOptions(url, select) {
   }
 };
 
-/* globals Chart:false, feather:false */
+//Código para recibir opciones de formulario EN CONSTRUCCIÓN
 
+const form = document.querySelector("#filtro-informe-form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  // Obtener los datos del formulario.
+  const empleadoSelect = form.elements.empleadoSelect.value;
+  const permisoSelect = form.elements.permisoSelect.value;
+  const fecha_desde = form.elements.fechaDesde.value;
+  const fecha_hasta = form.elements.fechaHasta.value;
+
+});
+
+//Convertir a CSV
+function tableToCSV() {
+  // Variable to store the final csv data
+  var csv_data = [];
+
+  // Get each row data
+  var rows = document.getElementsByTagName("tr");
+  for (var i = 0; i < rows.length; i++) {
+    // Get each column data
+    var cols = rows[i].querySelectorAll("td,th");
+
+    // Stores each csv row data
+    var csvrow = [];
+    for (var j = 0; j < cols.length; j++) {
+      // Get the text data of each cell
+      // of a row and push it to csvrow
+      csvrow.push(cols[j].innerHTML);
+    }
+
+    // Combine each column value with comma
+    csv_data.push(csvrow.join(","));
+  }
+
+  // Combine each row data with new line character
+  csv_data = csv_data.join("\n");
+
+  // Call this function to download csv file
+  downloadCSVFile(csv_data);
+};
+
+function downloadCSVFile(csv_data) {
+  // Create CSV file object and feed
+  // our csv_data into it
+  CSVFile = new Blob([csv_data], {
+    type: "text/csv;charset=utf-8",
+  });
+
+  // Create to temporary link to initiate
+  // download process
+  var temp_link = document.createElement("a");
+
+  // Download csv file
+  temp_link.download = "registros.csv";
+  var url = window.URL.createObjectURL(CSVFile);
+  temp_link.href = url;
+
+  // This link should not be displayed
+  temp_link.style.display = "none";
+  document.body.appendChild(temp_link);
+
+  // Automatically click the link to
+  // trigger download
+  temp_link.click();
+  document.body.removeChild(temp_link);
+};
+
+//Convertir a PDF
+function tableToPDF() {
+  var sTable = document.getElementsByTagName("table")[0];
+  var style = "<style>";
+  style = style + "table {width: 100%;font: 17px Calibri;}";
+  style =
+    style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+  style = style + "padding: 2px 3px;text-align: center;}";
+  style = style + "</style>";
+
+  // CREATE A WINDOW OBJECT.
+  var win = window.open("", "", "height=700,width=700");
+
+  win.document.write("<html><head>");
+  win.document.write("<title>Print</title>"); // <title> FOR PDF HEADER.
+  win.document.write(style); // ADD STYLE INSIDE THE HEAD TAG.
+  win.document.write("</head><body>");
+  win.document.write("<table>" + sTable.innerHTML + "</table>"); // THE TABLE CONTENTS INSIDE THE BODY TAG.
+  win.document.write("</body></html>");
+  win.document.close(); // CLOSE THE CURRENT WINDOW.
+
+  win.print(); // PRINT THE CONTENTS.
+}
+
+  
+//Se invoca funcion para cargar iconos de menu lateral rapidamente
+/* globals Chart:false, feather:false */
 (() => {
   "use strict";
 
