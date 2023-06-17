ce1146dd58f681da991429a235b6691b1d720d91
jvera <jvera@sentra.cl>
2023-06-17 19:42:51 -0400

    API recuperacion contraseña finalizado

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: '34.133.89.224',
-  database: 'sacdb',
-  password: 'Florentino25##',
+  host: 'localhost',
+  database: 'sac_local',
+  password: '0000001',
   port: 5432,
 });
 
