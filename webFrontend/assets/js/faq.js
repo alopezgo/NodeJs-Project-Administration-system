337e56fc76ad3f1fd9640419f95b172626fe7e55
jvera <jvera@sentra.cl>
2023-06-24 19:44:38 -0400

    Boton FAQ index + Faq function nombre

--- webFrontend/assets/js/faq.js
+++ webFrontend/assets/js/faq.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tokenUsuario = localStorage.getItem("token");
+    const isLoginPage = window.location.href.includes("login.html");
+  
+    if (!tokenUsuario && !isLoginPage) {
+      window.location.href = "login.html";
+    }
+    const nombreUsuario = localStorage.getItem("nombre_usuario");
+  
+    if (nombreUsuario) {
+      const nombreUsuarioElemento = document.getElementById("nombre-usuario");
+  
+      const imagenElemento = document.getElementById("img-usuario");
+      const nombreElemento = document.createElement("span");
+      nombreElemento.innerHTML = "Hola " + nombreUsuario;
+  
+      nombreUsuarioElemento.appendChild(imagenElemento);
+      nombreUsuarioElemento.appendChild(nombreElemento);
+    }
+  });
+
 function toggleAnswer(index) {
       var answer = document.getElementById('answer-' + index);
       
