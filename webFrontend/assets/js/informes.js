f2357e78ece0d2807a38df17ab6ee4b75353670e
alopezgo <alopezgo1991@gmail.com>
2023-06-25 00:05:37 -0400

    ordena código y comenta informes.js

--- webFrontend/assets/js/informes.js
+++ webFrontend/assets/js/informes.js
@@ -1,297 +1,277 @@
-(() => {
-  "use strict";
-
-  feather.replace({ "aria-hidden": "true" });
-  
-})();
-
-
-
 document.addEventListener("DOMContentLoaded", async function () {
-  const tokenUsuario = localStorage.getItem("token");
-  const isLoginPage = window.location.href.includes("login.html");
 
-  if (!tokenUsuario && !isLoginPage) {
+  //Se valida que el usuario haya iniciado sesión 
+  const tokenUsuario = localStorage.getItem("token");
+  if (!tokenUsuario) {
     window.location.href = "login.html";
   }
-  const nombreUsuario = localStorage.getItem("nombre_usuario");
+  else{
+    //Se invoca funcion para cargar iconos de menu lateral rapidamente
+    "use strict";
+    feather.replace({ "aria-hidden": "true" });
 
-  if (nombreUsuario) {
+    //Se rellena menu lateral con Nombre del Usuario
+    const nombreUsuario = localStorage.getItem("nombre_usuario");
     const nombreUsuarioElemento = document.getElementById("nombre-usuario");
-
-    const imagenElemento = document.getElementById("img-usuario");
     const nombreElemento = document.createElement("span");
-    nombreElemento.innerHTML = "Hola " + nombreUsuario;
 
-    nombreUsuarioElemento.appendChild(imagenElemento);
+    if (nombreUsuario != "") {
+      nombreElemento.innerHTML = "Hola " + nombreUsuario;
+    } 
+    else {
+      nombreElemento.innerHTML = "Hola";
+    }
+
     nombreUsuarioElemento.appendChild(nombreElemento);
-  }
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var ctxl = document.getElementById("myChart2").getContext('2d');
-var ctx3 = document.getElementById("myChart3").getContext('2d');
-var ctx4 = document.getElementById("myChart4").getContext('2d');
-var ctx5 = document.getElementById("myChart5").getContext('2d');
-var ctx6 = document.getElementById("myChart6").getContext('2d');
+    //Obtiene elementos tipo canvas para renderizar los gráficos
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var ctxl = document.getElementById("myChart2").getContext('2d');
+    var ctx3 = document.getElementById("myChart3").getContext('2d');
+    var ctx4 = document.getElementById("myChart4").getContext('2d');
+    var ctx5 = document.getElementById("myChart5").getContext('2d');
+    var ctx6 = document.getElementById("myChart6").getContext('2d');
 
-const id_empresa = localStorage.getItem("id_empresa");
+    // Objeto de traducciones de inglés a español para los meses
+    const monthTranslations = {
+      1: "Enero",
+      2: "Febrero",
+      3: "Marzo",
+      4: "Abril",
+      5: "Mayo",
+      6: "Junio",
+      7: "Julio",
+      8: "Agosto",
+      9: "Septiembre",
+      10: "Octubre",
+      11: "Noviembre",
+      12: "Diciembre"
+    };
 
-  // Objeto de traducciones de inglés a español para los meses
-const monthTranslations = {
-  1: "Enero",
-  2: "Febrero",
-  3: "Marzo",
-  4: "Abril",
-  5: "Mayo",
-  6: "Junio",
-  7: "Julio",
-  8: "Agosto",
-  9: "Septiembre",
-  10: "Octubre",
-  11: "Noviembre",
-  12: "Diciembre"
-};
+    try {
+      const data = await GetInformeConsumoMensual();
+      var labels = data.map(item => monthTranslations[item.mes] || item.mes);
+      var valuesTotal = data.map(item => item.total);
+      var valuesCantidad = data.map(item => item.cant_consumos);
 
-  try {
-    const data = await GetInformeConsumoMensual();
-    var labels = data.map(item => monthTranslations[item.mes] || item.mes);
-    var valuesTotal = data.map(item => item.total);
-    var valuesCantidad = data.map(item => item.cant_consumos);
-
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: `Total en $ por consumo mensual`,
-          data: valuesTotal,
-          backgroundColor: [
-            // 'rgba(255, 99, 132, 0.2)',
-            // 'rgba(54, 162, 235, 0.2)',
-            // 'rgba(255, 206, 86, 0.2)',
-            // 'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            //'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            // 'rgba(255,99,132,1)',
-            // 'rgba(54, 162, 235, 1)',
-            // 'rgba(255, 206, 86, 1)',
-            // 'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            //'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+      //Primer gráfico Consumos
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: `Total en $ por consumo mensual`,
+            data: valuesTotal,
+            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
+            borderColor: ['rgba(153, 102, 255, 1)'],
+            borderWidth: 1
           }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
         }
-      }
-    });
-    //line
-var myLineChart = new Chart(ctxl, {
-  type: 'line',
-  data: {
-    labels: labels,
-    datasets: [{
-      label: "Cantidad de consumos por mes",
-      data: valuesCantidad,
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
-      borderWidth: 2
-    }
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
+      });
+      //Segundo gráfico Consumos
+      var myLineChart = new Chart(ctxl, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: "Cantidad de consumos por mes",
+            data: valuesCantidad,
+            backgroundColor: [
+              'rgba(105, 0, 132, .2)',
+            ],
+            borderColor: [
+              'rgba(200, 99, 132, .7)',
+            ],
+            borderWidth: 2
+          }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
         }
-      }]
+      });
+    } catch (error) {
+      console.error('Error:', error);
     }
+    try {
+      const data = await GetMetricas();
+      var labels = data.map(item => monthTranslations[item.mes] || item.mes);
+      var valuesAsistEsperada = data.map(item => item.asistencia_esperada);
+      var valuesPorcAsist = data.map(item => item.porc_obj_asist);
+      var valuesAsistReal = data.map(item => item.asistencia_real);
+      var q_emp_asiste = data.map(item => item.q_emp_asiste);
+      var q_emp_consume = data.map(item => item.q_emp_consume);
+      var valuesAlmReal = data.map(item => item.almuerzos_real);
+      var valuesDeltaAlm = data.map(item => item.delta_almuerzos);
+  
+      //Primer Gráfico Alimentacion
+      var myLineChart = new Chart(ctx3, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: "Asistencia Esperada",
+            data: valuesAsistEsperada,
+            backgroundColor: ['rgba(105, 0, 132, .2)'],
+            borderColor: ['rgba(200, 99, 132, .7)'],
+            borderWidth: 2
+          },
+          {
+            label: "Asistencia Real",
+            data: valuesAsistReal,
+            backgroundColor: [
+              'rgba(0, 137, 132, .2)',
+            ],
+            borderColor: [
+              'rgba(0, 10, 130, .7)',
+            ],
+            borderWidth: 2
+          }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+      //Segundo Gráfico Alimentacion
+      var myHorizChart = new Chart(ctx4,  {
+        "type": "bar",
+        "data": {
+          "labels": labels,
+          "datasets": [{
+            "label": "Porcentaje cumplimiento Asistencia Esperada vs Real",
+            "data": valuesPorcAsist,
+            "fill": false,
+            "backgroundColor": ["rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)",
+              "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)", "rgba(255, 99, 132, 0.2)", 
+            ],
+            "borderColor": ["rgb(255, 159, 64)", "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)",
+              "rgb(255, 99, 132)", 
+            ],
+            "borderWidth": 1
+          }]
+        },
+        "options": {
+          "scales": {
+            "xAxes": [{
+              "ticks": {
+                "beginAtZero": true
+              }
+            }]
+          }
+        }
+      });
+      //Primer Gráfico Global
+      var myChart5 = new Chart(ctx5,  {
+        "type": "line",
+        "data": {
+          "labels": labels,
+          "datasets": [{
+            "label": "Asistencia Real",
+            "data": valuesAsistReal,
+            "fill": false,
+            "backgroundColor": ["rgba(127, 63, 191, 0.2)"],
+            "borderColor": ["rgb(127, 63, 191)"],
+            "borderWidth": 1
+          },
+          {
+            "label": "Almuerzos Real",
+            "data": valuesAlmReal,
+            "fill": false,
+            "backgroundColor": ["rgba(255, 99, 132, 0.2)"],
+            "borderColor": ["rgb(255, 99, 132)"],
+            "borderWidth": 1
+          },
+          {
+            "label": "Delta Almuerzos",
+            "data": valuesDeltaAlm,
+            "fill": false,
+            "backgroundColor": ["rgba(63, 191, 191, 0.2)"],
+            "borderColor": ["rgb(63, 191, 191)"],
+            "borderWidth": 1
+          }]
+        },
+        "options": {
+          "scales": {
+            "xAxes": [{
+              "ticks": {
+                "beginAtZero": true
+              }
+            }]
+          }
+        }
+      });
+      var myChart6 = new Chart(ctx6,  {
+        "type": "bar",
+        "data": {
+          "labels": labels,
+          "datasets": [{
+            "label": "Q empleados que almuerzan",
+            "data": q_emp_consume,
+            "fill": false,
+            "backgroundColor": [
+              "rgba(63, 191, 127, 0.2)"
+            ],
+            "borderColor": ["rgb(63, 191, 127)"],
+            "borderWidth": 1
+          },
+          {
+            "label": "Q empleados que asisten",
+            "data": q_emp_asiste,
+            "fill": false,
+            "backgroundColor": ["rgba(4, 76, 51, 0.2)"],
+            "borderColor": ["rgb(4, 153, 102)"],
+            "borderWidth": 1
+          }]
+        },
+        "options": {
+          "scales": {
+            "xAxes": [{
+              "ticks": {
+                "beginAtZero": true
+              }
+            }]
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    try {
+      const data = await GetMetricas();
+      createTable(data);
+    } catch (error) {
+      console.error(error);
+    }
+
   }
 });
-  } catch (error) {
-    console.error('Error:', error);
-  }
 
-  try {
-    const data = await GetMetricas();
-    var labels = data.map(item => monthTranslations[item.mes] || item.mes);
-    var valuesAsistEsperada = data.map(item => item.asistencia_esperada);
-    var valuesPorcAsist = data.map(item => item.porc_obj_asist);
-    var valuesAsistReal = data.map(item => item.asistencia_real);
-    var valuesPermisos = data.map(item => item.permisos);
-    var q_emp_asiste = data.map(item => item.q_emp_asiste);
-    var q_emp_consume = data.map(item => item.q_emp_consume);
-    var valuesAlmReal = data.map(item => item.almuerzos_real);
-    var valuesDeltaAlm = data.map(item => item.delta_almuerzos);
 
-    //line
-var myLineChart = new Chart(ctx3, {
-  type: 'line',
-  data: {
-    labels: labels,
-    datasets: [{
-      label: "Asistencia Esperada",
-      data: valuesAsistEsperada,
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
-      borderWidth: 2
-    },
-    {
-      label: "Asistencia Real",
-      data: valuesAsistReal,
-      backgroundColor: [
-        'rgba(0, 137, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(0, 10, 130, .7)',
-      ],
-      borderWidth: 2
-    }
-
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
-var myHorizChart = new Chart(ctx4,  {
-  "type": "bar",
-  "data": {
-    "labels": labels,
-    "datasets": [{
-      "label": "Porcentaje cumplimiento Asistencia Esperada vs Real",
-      "data": valuesPorcAsist,
-      "fill": false,
-      "backgroundColor": ["rgba(255, 159, 64, 0.2)",
-        "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)",
-        "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)", "rgba(255, 99, 132, 0.2)", 
-      ],
-      "borderColor": ["rgb(255, 159, 64)", "rgb(255, 205, 86)",
-        "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)",
-        "rgb(255, 99, 132)", 
-      ],
-      "borderWidth": 1
-    }]
-  },
-  "options": {
-    "scales": {
-      "xAxes": [{
-        "ticks": {
-          "beginAtZero": true
-        }
-      }]
-    }
-  }
-});
-
-var myChart5 = new Chart(ctx5,  {
-  "type": "line",
-  "data": {
-    "labels": labels,
-    "datasets": [{
-      "label": "Asistencia Real",
-      "data": valuesAsistReal,
-      "fill": false,
-      "backgroundColor": ["rgba(127, 63, 191, 0.2)"],
-      "borderColor": ["rgb(127, 63, 191)"],
-      "borderWidth": 1
-    },
-    {
-      "label": "Almuerzos Real",
-      "data": valuesAlmReal,
-      "fill": false,
-      "backgroundColor": ["rgba(255, 99, 132, 0.2)"],
-      "borderColor": ["rgb(255, 99, 132)"],
-      "borderWidth": 1
-    },
-    {
-      "label": "Delta Almuerzos",
-      "data": valuesDeltaAlm,
-      "fill": false,
-      "backgroundColor": ["rgba(63, 191, 191, 0.2)"],
-      "borderColor": ["rgb(63, 191, 191)"],
-      "borderWidth": 1
-    }]
-  },
-  "options": {
-    "scales": {
-      "xAxes": [{
-        "ticks": {
-          "beginAtZero": true
-        }
-      }]
-    }
-  }
-});
-var myChart6 = new Chart(ctx6,  {
-  "type": "bar",
-  "data": {
-    "labels": labels,
-    "datasets": [{
-      "label": "Q empleados que almuerzan",
-      "data": q_emp_consume,
-      "fill": false,
-      "backgroundColor": [
-        "rgba(63, 191, 127, 0.2)"
-      ],
-      "borderColor": ["rgb(63, 191, 127)"],
-      "borderWidth": 1
-    },
-    {
-      "label": "Q empleados que asisten",
-      "data": q_emp_asiste,
-      "fill": false,
-      "backgroundColor": ["rgba(4, 76, 51, 0.2)"],
-      "borderColor": ["rgb(4, 153, 102)"],
-      "borderWidth": 1
-    }]
-  },
-  "options": {
-    "scales": {
-      "xAxes": [{
-        "ticks": {
-          "beginAtZero": true
-        }
-      }]
-    }
-  }
-});
-  } catch (error) {
-    console.error('Error:', error);
-  }
-
-  try {
-    const data = await GetMetricas();
-    createTable(data);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
+//Función Auxiliar para obtener datos del primer y segundo gráfico Consumo
 async function GetInformeConsumoMensual() {
   const id_empresa = localStorage.getItem("id_empresa");
   let url = `http://localhost:3000/api/v1/consumos/informeconsumos/${id_empresa}?`;
@@ -310,7 +290,7 @@ async function GetInformeConsumoMensual() {
     alert("Error en servicio informeconsumos");
   }
 }
-
+//Función Auxiliar para obtener datos de los demás gráficos
 async function GetMetricas() {
   const id_empresa = localStorage.getItem("id_empresa");
   let url = `http://localhost:3000/api/v1/informemetricas/${id_empresa}?`;
@@ -330,6 +310,7 @@ async function GetMetricas() {
   }
 }
 
+//Función Auxiliar crear tabla con méttricas
 function createTable(data) {
   const table = document.getElementById("table-metricas");
   const tableHeader = document.createElement("thead");
