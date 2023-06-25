9f9a9fdb324a7d80b04ea77e1b8190875e4fb754
alopezgo <alopezgo1991@gmail.com>
2023-06-25 02:02:15 -0400

    Ordena/comenta asistencia.js y agrega mensaje cargando al crear tabla

--- webFrontend/assets/js/asistencia.js
+++ webFrontend/assets/js/asistencia.js
@@ -1,81 +1,126 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tokenUsuario = localStorage.getItem("token");
-  const isLoginPage = window.location.href.includes("login.html");
 
-  if (!tokenUsuario && !isLoginPage) {
+   //Se valida que el usuario haya iniciado sesión 
+  const tokenUsuario = localStorage.getItem("token");
+  if (!tokenUsuario) {
     window.location.href = "login.html";
   }
-  var tabla_asistencia = $("#example").DataTable({
-    language: {
-      url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
-    },
-    columns: [
-      { title: "Centro", data: "centro_costos" },
-      { title: "Empleado", data: "empleado" },
-      { title: "Rut", data: "rut" },
-      { title: "Tipo Asistencia", data: "tipo" },
-      { title: "Fecha", data: "fecha" },
-      { title: "Hora", data: "hora" },
-    ],
-  });
-  GetAsistencias();
-  // Obtener lista de Asistencias por Empresa
-  function GetAsistencias() {
-    const id_empresa = localStorage.getItem("id_empresa");
-    return $.ajax({
-      url: "http://localhost:3000/api/v1/asistencias/" + id_empresa,
-      type: "GET",
-      success: function (response) {
-        const res = response;
-        const asistencia = res.data;
-        asistencia.forEach((fila) => {
-          tabla_asistencia.row
-            .add({
-              centro_costos: fila.centro_costos,
-              empleado: fila.empleado,
-              rut: fila.rut,
-              tipo: fila.tipo,
-              fecha: fila.fecha,
-              hora: fila.hora,
-            })
-            .draw();
-        });
+  else {
+    //Se invoca funcion para cargar iconos de menu lateral rapidamente
+    "use strict";
+    feather.replace({ "aria-hidden": "true" });
+
+    //Se rellena menu lateral con Nombre del Usuario
+    const nombreUsuario = localStorage.getItem("nombre_usuario");
+    const nombreUsuarioElemento = document.getElementById("nombre-usuario");
+    const nombreElemento = document.createElement("span");
+
+    if (nombreUsuario != "") {
+      nombreElemento.innerHTML = "Hola " + nombreUsuario;
+    } 
+    else {
+      nombreElemento.innerHTML = "Hola";
+    }
+
+    nombreUsuarioElemento.appendChild(nombreElemento);
+
+    //Se llama a función GetAsistencias para traer datos en la tabla
+    var tabla_asistencia = $("#example").DataTable({
+      language: {
+        url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
       },
+      columns: [
+        { title: "Centro", data: "centro_costos" },
+        { title: "Empleado", data: "empleado" },
+        { title: "Rut", data: "rut" },
+        { title: "Tipo Asistencia", data: "tipo" },
+        { title: "Fecha", data: "fecha" },
+        { title: "Hora", data: "hora" },
+      ],
     });
+    try{
+      GetAsistencias(tabla_asistencia);
+    }catch(error){
+      console.error(error);
+      alert("Error al traer registros de asistencia");
+    }
+  
+    //Se llama a función cargarOptions para obtener options en select de centro costos y tipo asistencia
+    const id_empresa = localStorage.getItem("id_empresa");
+    const url_centros = "http://localhost:3000/api/v1/centrocosto/" + id_empresa;
+    const url_tiposAsistencia = "http://localhost:3000/api/v1/tiposasistencia";
+    const centroSelect = document.querySelector("#centroSelect");
+    const asistenciaSelect = document.querySelector("#asistenciaSelect");
+    try{
+      cargarOptions(url_centros, centroSelect);
+      cargarOptions(url_tiposAsistencia, asistenciaSelect);
+    }catch(error){
+      console.error(error);
+      alert("Error al cargar opciones en formulario");
+    }
+    
+  
+    //Se rellenan opciones de inputs tipo date en Formulario
+    var fecha = new Date(); //Fecha actual
+    var mes = fecha.getMonth() + 1; //obteniendo mes
+    var dia = fecha.getDate(); //obteniendo dia
+    var ano = fecha.getFullYear(); //obteniendo año
+    if (dia < 10) dia = "0" + dia; //agrega cero si el menor de 10
+    if (mes < 10) mes = "0" + mes; //agrega cero si el menor de 10
+    document.getElementById("fechaDesde").value = ano + "-" + mes + "-" + dia;
+    document.getElementById("fechaHasta").value = ano + "-" + mes + "-" + dia;
+  
+  }
+  
+});
+
+//Se obtiene elemento formulario
+const form = document.querySelector("#filtro-informe-form");
+//cuando ocurra el evento submit se llama a la api asistencia nuevamente
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  // Se obtienen valores del formulario
+  const centro_costos = form.elements.centroSelect.value;
+  const tipo = form.elements.asistenciaSelect.value;
+  const fecha_desde = form.elements.fechaDesde.value;
+  const fecha_hasta = form.elements.fechaHasta.value;
+
+  //convierte fechas a date para validar que fecha desde es menor o igual a fecha_hasta
+  const fechaDesdeObj = new Date(fecha_desde);
+  const fechaHastaObj = new Date(fecha_hasta);
+  if (fechaDesdeObj > fechaHastaObj) {
+    alert("La fecha desde no puede ser mayor que la fecha hasta");
+  }
+  else{
+    //Si la tabla ya existe se elimina informacion y se borra la tabla
+    if ($.fn.dataTable.isDataTable("#example")) {
+      $("#example").dataTable().fnClearTable();
+      $("#example").dataTable().fnDestroy();
+    }
+
+    table = $("#example").DataTable({
+      language: {
+        destroy: true,
+        url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+      },
+      columns: [
+        { title: "Centro", data: "centro_costos" },
+        { title: "Empleado", data: "empleado" },
+        { title: "Rut", data: "rut" },
+        { title: "Tipo Asistencia", data: "tipo" },
+        { title: "Fecha", data: "fecha" },
+        { title: "Hora", data: "hora" },
+      ],
+    });
+
+    try {
+      GetAsistenciasPorParametros(centro_costos, tipo, fecha_desde, fecha_hasta);
+    }catch(error){
+      console.error(error);
+      alert("Error al generar informe")
+    }
   }
 
-  //Se rellenan fechas de inputs tipo select en Formulario
-  const id_empresa = localStorage.getItem("id_empresa");
-  const url_centros = "http://localhost:3000/api/v1/centrocosto/" + id_empresa;
-  const url_tiposAsistencia = "http://localhost:3000/api/v1/tiposasistencia";
-  const centroSelect = document.querySelector("#centroSelect");
-  const asistenciaSelect = document.querySelector("#asistenciaSelect");
-  cargarOptions(url_centros, centroSelect);
-  cargarOptions(url_tiposAsistencia, asistenciaSelect);
-
-  //Se rellenan opciones de inputs tipo date en Formulario
-  var fecha = new Date(); //Fecha actual
-  var mes = fecha.getMonth() + 1; //obteniendo mes
-  var dia = fecha.getDate(); //obteniendo dia
-  var ano = fecha.getFullYear(); //obteniendo año
-  if (dia < 10) dia = "0" + dia; //agrega cero si el menor de 10
-  if (mes < 10) mes = "0" + mes; //agrega cero si el menor de 10
-  document.getElementById("fechaDesde").value = ano + "-" + mes + "-" + dia;
-  document.getElementById("fechaHasta").value = ano + "-" + mes + "-" + dia;
-
-  //Se rellena menu lateral con Nombre del Usuario
-  const nombreUsuario = localStorage.getItem("nombre_usuario");
-  const nombreUsuarioElemento = document.getElementById("nombre-usuario");
-  const imagenElemento = document.getElementById("img-usuario");
-  const nombreElemento = document.createElement("span");
-
-  if (nombreUsuario != "") {
-    nombreElemento.innerHTML = "Hola " + nombreUsuario;
-  } else {
-    nombreElemento.innerHTML = "Mi cuenta";
-  }
-  nombreUsuarioElemento.appendChild(imagenElemento);
-  nombreUsuarioElemento.appendChild(nombreElemento);
 });
 
 // Función auxiliar para cargar options en Select.
@@ -142,127 +187,112 @@ async function cargarOptions(url, select) {
   }
 };
 
-const form = document.querySelector("#filtro-informe-form");
-
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  // Obtener los datos del formulario.
-  const centro_costos = form.elements.centroSelect.value;
-  const tipo = form.elements.asistenciaSelect.value;
-  const fecha_desde = form.elements.fechaDesde.value;
-  const fecha_hasta = form.elements.fechaHasta.value;
-
-  if ($.fn.dataTable.isDataTable("#example")) {
-    $("#example").dataTable().fnClearTable();
-    $("#example").dataTable().fnDestroy();
-  }
-
-  table = $("#example").DataTable({
-    language: {
-      destroy: true,
-      url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+// Función auxiliar para obtener lista de Asistencias por Empresa
+function GetAsistencias(tabla_asistencia) {
+  const id_empresa = localStorage.getItem("id_empresa");
+  // Mostrar mensaje de carga
+  const loadingMessage = document.getElementById("cargando");
+  loadingMessage.textContent = "Cargando...";
+  return $.ajax({
+    url: "http://localhost:3000/api/v1/asistencias/" + id_empresa,
+    type: "GET",
+    success: function (response) {
+      const res = response;
+      const asistencia = res.data;
+      asistencia.forEach((fila) => {
+        tabla_asistencia.row
+          .add({
+            centro_costos: fila.centro_costos,
+            empleado: fila.empleado,
+            rut: fila.rut,
+            tipo: fila.tipo,
+            fecha: fila.fecha,
+            hora: fila.hora,
+          })
+          .draw();
+      });
+       // Eliminar mensaje de carga
+       loadingMessage.remove();
     },
-    columns: [
-      { title: "Centro", data: "centro_costos" },
-      { title: "Empleado", data: "empleado" },
-      { title: "Rut", data: "rut" },
-      { title: "Tipo Asistencia", data: "tipo" },
-      { title: "Fecha", data: "fecha" },
-      { title: "Hora", data: "hora" },
-    ],
   });
+}
 
-  await GetAsistenciasPorParametros();
-  function GetAsistenciasPorParametros() {
-    const id_empresa = localStorage.getItem("id_empresa");
-    let url_params = `http://localhost:3000/api/v1/asistencias/${id_empresa}?`;
-    if (centro_costos != 0) {
-      url_params += `&centro=${centro_costos}`;
-    }
-    if (tipo != 0) {
-      url_params += `&evento=${tipo}`;
-    }
-    url_params += `&desde=${fecha_desde}&hasta=${fecha_hasta}`;
-    return $.ajax({
-      url: url_params,
-      type: "GET",
-      success: function (response) {
-        const res = response;
-        const consumo = res.data;
-        consumo.forEach((fila) => {
-          table.row
-            .add({
-              centro_costos: fila.centro_costos,
-              empleado: fila.empleado,
-              rut: fila.rut,
-              tipo: fila.tipo,
-              fecha: fila.fecha,
-              hora: fila.hora,
-            })
-            .draw();
-        });
-      },
-    });
+// Función Auxiliar para Obtener lista de asistencias aplicando filtros del Formulario
+function GetAsistenciasPorParametros(centro_costos,tipo, fecha_desde, fecha_hasta) {
+  const id_empresa = localStorage.getItem("id_empresa");
+  // Mostrar mensaje de carga
+  let cargaMensaje = document.getElementById("cargando2");
+  cargaMensaje.textContent = "Cargando...";
+  let url_params = `http://localhost:3000/api/v1/asistencias/${id_empresa}?`;
+  if (centro_costos != 0) {
+    url_params += `&centro=${centro_costos}`;
   }
-});
+  if (tipo != 0) {
+    url_params += `&evento=${tipo}`;
+  }
+  url_params += `&desde=${fecha_desde}&hasta=${fecha_hasta}`;
+  return $.ajax({
+    url: url_params,
+    type: "GET",
+    success: function (response) {
+      const res = response;
+      const consumo = res.data;
+      consumo.forEach((fila) => {
+        table.row
+          .add({
+            centro_costos: fila.centro_costos,
+            empleado: fila.empleado,
+            rut: fila.rut,
+            tipo: fila.tipo,
+            fecha: fila.fecha,
+            hora: fila.hora,
+          })
+          .draw();
+      });
+      // Eliminar mensaje de carga
+      cargaMensaje.textContent="";
+    },
+  });
+}
 
-//Convertir a CSV
+//Función auxiliar para convertir tabla a CSV
 function tableToCSV() {
-  // Variable to store the final csv data
+ 
   var csv_data = [];
-
-  // Get each row data
   var rows = document.getElementsByTagName("tr");
   for (var i = 0; i < rows.length; i++) {
-    // Get each column data
     var cols = rows[i].querySelectorAll("td,th");
-
-    // Stores each csv row data
     var csvrow = [];
     for (var j = 0; j < cols.length; j++) {
-      // Get the text data of each cell
-      // of a row and push it to csvrow
       csvrow.push(cols[j].innerHTML);
     }
 
-    // Combine each column value with comma
     csv_data.push(csvrow.join(","));
   }
 
-  // Combine each row data with new line character
   csv_data = csv_data.join("\n");
-
-  // Call this function to download csv file
   downloadCSVFile(csv_data);
 };
 
+//Función auxiliar para descargar CSV
 function downloadCSVFile(csv_data) {
-  // Create CSV file object and feed
-  // our csv_data into it
   CSVFile = new Blob([csv_data], {
     type: "text/csv;charset=utf-8",
   });
 
-  // Create to temporary link to initiate
-  // download process
   var temp_link = document.createElement("a");
-
-  // Download csv file
   temp_link.download = "registros.csv";
   var url = window.URL.createObjectURL(CSVFile);
   temp_link.href = url;
 
-  // This link should not be displayed
   temp_link.style.display = "none";
   document.body.appendChild(temp_link);
 
-  // Automatically click the link to
-  // trigger download
   temp_link.click();
   document.body.removeChild(temp_link);
 }
 
-//Convertir a PDF
+//Función auxiliar para convertir tabla en nueva ventana y poder imprimir o guardar como pdf
 function tableToPDF() {
   var sTable = document.getElementsByTagName("table")[0];
   var style = "<style>";
@@ -285,11 +315,3 @@ function tableToPDF() {
 
   win.print(); // PRINT THE CONTENTS.
 }
-
-//Se invoca funcion para cargar iconos de menu lateral rapidamente
-/* globals Chart:false, feather:false */
-(() => {
-  "use strict";
-
-  feather.replace({ "aria-hidden": "true" });
-})();
